380e35b6f0e449f3d79a62ba8e1ed446c25506d9
joevandyk <joevandyk@ca20866a-8536-0410-a432-c7584e41aef3>
2007-07-31 07:50:54 +0000

    fix
    git-svn-id: https://monkeycharger.googlecode.com/svn@24 ca20866a-8536-0410-a432-c7584e41aef3

--- trunk/vendor/plugins/monkey_charger/lib/capturer.rb
+++ trunk/vendor/plugins/monkey_charger/lib/capturer.rb
@@ -1,7 +1,9 @@
+class BillingError < StandardError; end
+
 class Capturer
    SITE = "http://localhost:3001/captures"
-   def self.capture! transaction_id
-      response = Net::HTTP.post_form URI.parse(SITE), :transaction_id => transaction_id 
+   def self.capture! amount, transaction_id
+      response = Net::HTTP.post_form URI.parse(SITE), :amount => amount, :transaction_id => transaction_id 
       case response
       when Net::HTTPSuccess
          raise BillingError, response.body unless response.header['X-CaptureSuccess'] 
