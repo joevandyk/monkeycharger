4a620cc58c48d0e784e18e943296f36dbf70141c
joevandyk <joevandyk@ca20866a-8536-0410-a432-c7584e41aef3>
2007-07-31 03:17:12 +0000

    added test documentation
    git-svn-id: https://monkeycharger.googlecode.com/svn@8 ca20866a-8536-0410-a432-c7584e41aef3

--- trunk/app/controllers/authorizations_controller.rb
+++ trunk/app/controllers/authorizations_controller.rb
@@ -1,9 +1,9 @@
 class AuthorizationsController < ApplicationController
   def create
      @credit_card = 
-        if params[:credit_card_id]
+        if params[:credit_card_id]  # We're charging an existing card
            CreditCard.find params[:credit_card_id] 
-        else
+        else                        # We're charging a new card.  (not gonna save it though)
            CreditCard.new(:number => params[:number], :cvv => params[:cvv], :month => params[:month], :year => params[:year])
         end
      transaction_id =  Authorizer::authorize!(:amount => params[:amount], :credit_card => @credit_card)
