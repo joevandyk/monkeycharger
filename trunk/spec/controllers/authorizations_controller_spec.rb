d9f6ac97bfdb9574011358959b00f186856940b4
joevandyk <joevandyk@ca20866a-8536-0410-a432-c7584e41aef3>
2007-07-31 11:35:52 +0000

    added tests
    git-svn-id: https://monkeycharger.googlecode.com/svn@35 ca20866a-8536-0410-a432-c7584e41aef3

--- trunk/spec/controllers/authorizations_controller_spec.rb
+++ trunk/spec/controllers/authorizations_controller_spec.rb
@@ -4,9 +4,11 @@ describe AuthorizationsController, "authorizing a non-saved card" do
    before(:each) do
       @auth_id = 'authid'
       @credit_card = CreditCard.new(valid_cc_data)
+      remote_key = '12345'
+      @credit_card.should_receive(:decrypt).with(remote_key)
       CreditCard.should_receive(:new).with(valid_cc_data).and_return(@credit_card)
       Authorizer.should_receive(:authorize!).with({:credit_card => @credit_card, :amount => '3.99'}).and_return(@auth_id)
-      post_parameters = {:amount => '3.99'}.merge(valid_cc_data)
+      post_parameters = {:amount => '3.99', :remote_key => '12345'}.merge(valid_cc_data)
       post :create, post_parameters
    end
 
@@ -32,8 +34,10 @@ describe "A successful authorization of a saved card" do
       @credit_card = generate_credit_card
       CreditCard.should_receive(:find).with(@credit_card.id.to_s).and_return(@credit_card)
       @auth_id = 'auth_id'
+      remote_key = '12345'
+      @credit_card.should_receive(:decrypt).with(remote_key)
       Authorizer.should_receive(:authorize!).with(:amount => '3.99', :credit_card => @credit_card).and_return(@auth_id)
-      post :create, :amount => '3.99', :credit_card_id => @credit_card.id
+      post :create, :amount => '3.99', :credit_card_id => @credit_card.id, :remote_key => remote_key
    end
 
    it "the AuthorizationSuccess header should be set" do
