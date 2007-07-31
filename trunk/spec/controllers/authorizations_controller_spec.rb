48b9bca477ee9709907d9240a498a86adcacc3ec
joevandyk <joevandyk@ca20866a-8536-0410-a432-c7584e41aef3>
2007-07-31 03:24:29 +0000

    made the tests beautiful
    git-svn-id: https://monkeycharger.googlecode.com/svn@9 ca20866a-8536-0410-a432-c7584e41aef3

--- trunk/spec/controllers/authorizations_controller_spec.rb
+++ trunk/spec/controllers/authorizations_controller_spec.rb
@@ -5,39 +5,60 @@ describe AuthorizationsController, "authorizing a non-saved card" do
    before(:each) do
       @credit_card = CreditCard.new(:number => '4111111111111111', :month => '9', :year => '2009', :cvv => '123')
       CreditCard.should_receive(:new).with(:number => @credit_card.number, :month => @credit_card.month, :year => @credit_card.year, :cvv => @credit_card.cvv).and_return(@credit_card)
-   end
-
-   it "should authorize a non-saved card" do
       Authorizer.should_receive(:authorize!).with({:credit_card => @credit_card, :amount => '3.99'}).and_return('authid')
       post :create, :amount => '3.99', :number => @credit_card.number, :cvv => @credit_card.cvv, :month => @credit_card.month, :year => @credit_card.year
-      response.should be_success
+   end
+
+   it "the body of the response should be set to the authorization id " do
       response.body.should == 'authid'
+   end
+
+   it "should be a success" do
+      response.should be_success
    end
 
 end
 
-describe AuthorizationsController, "authorizing a saved card" do
+describe "A successful authorization of a saved card" do
+   controller_name :authorizations
    fixtures :credit_cards
    before(:each) do
       @credit_card = credit_cards(:saved_card)
       CreditCard.should_receive(:find).with(@credit_card.id.to_s).and_return(@credit_card)
-   end
-
-   it "should authorize a saved card" do
       Authorizer.should_receive(:authorize!).with(:amount => '3.99', :credit_card => @credit_card).and_return('authid')
       post :create, :amount => '3.99', :credit_card_id => @credit_card.id
-      response.should be_success
-      response.body.should == 'authid'
+   end
+
+   it "the AuthorizationSuccess header should be set" do
       response.headers['X-AuthorizationSuccess'].should be_true
    end
 
-   it "should give an error when authorizing a bad card" do
-      Authorizer.should_receive(:authorize!).with(:amount => '3.99', :credit_card => @credit_card).and_raise(AuthorizationError.new("problem!"))
-      post :create, :amount => '3.99', :credit_card_id => @credit_card.id
-      response.headers['X-AuthorizationSuccess'].should_not be_true
-      response.body.should == 'problem!'
+   it "should be a success" do
+      response.should be_success
    end
 
+   it "the body of the response should be set to the authorization id" do
+      response.body.should == 'authid'
+   end
+end
+
+describe "A failed authorization of a saved card" do
+   controller_name :authorizations
+   fixtures :credit_cards
+   before(:each) do
+      @credit_card = credit_cards(:saved_card)
+      CreditCard.should_receive(:find).with(@credit_card.id.to_s).and_return(@credit_card)
+      Authorizer.should_receive(:authorize!).with(:amount => '3.99', :credit_card => @credit_card).and_raise(AuthorizationError.new("problem!"))
+      post :create, :amount => '3.99', :credit_card_id => @credit_card.id
+   end
+
+   it "should not set the AuthorizationSuccess header" do
+      response.headers['X-AuthorizationSuccess'].should_not be_true
+   end
+
+   it "the body of the response should be set to the reason why the authorization failed" do
+      response.body.should == 'problem!'
+   end
 end
 
 describe AuthorizationsController do
